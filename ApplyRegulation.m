09c6e37e24e38da45108459a2e481b552ab9961a
Claudio Mirabella <165279996+ClaudioMirabella@users.noreply.github.com>
2024-04-01 15:43:58 +0200

    Add files via upload

---
+++ ApplyRegulation.m
@@ -0,0 +1,77 @@
+function Aircraft = ApplyRegulation(Aircraft)
+%% DESCRIPTION OF THE FUNCTION
+% Aircraft = ApplyRegulation(Aircraft)
+%   This functions allows the user to automatically switch through various
+%   regulations, basing the switch activation on a char value stored inside
+%   a struct variable called 'Aircraft'. The field inside which the char
+%   value is stored is 
+%                    Aircraft.Certification.Regulation.value
+%   Possible switch case are:
+%   ---> CSVLA
+%   ---> CS22 (currently not available) 
+%   ---> CS23 (currently not available)
+%   Inside each case a properly defined script is called, to apply
+%   calculations methods stored inside class files. To manage in a more
+%   clear manner all the operations every case will change the working
+%   directory to the one which contains all the files needed to carry out
+%   the calculations. Outputs are stored inside the struct variable
+%   'Aircraft' and in a sub-folder called Output in .pdf files format.
+
+    %% SWITCH CASE TO SELECT APPLICABLE REGULATION 
+    switch (Aircraft.Certification.Regulation.value)
+        % CASE 1: Very Light Aircraft
+        case 'csvla'
+            % Change working directory
+            cd csvla
+            % The 'dir' variable contains working directory path saved as a
+            % char value
+            dir = pwd;
+            % Store working directory inside the log file
+            fprintf('-----------------');
+            fprintf('\n');
+            fprintf('### Current directory ###');
+            fprintf('\n');
+            fprintf('%s\n', dir);
+            % Call the class csvla.m
+            fprintf('-----------------');
+            fprintf('\n');
+            fprintf('### Flight Envelope - per CS - VLA ###');
+            fprintf('\n');
+
+            % WING WEIGHT CALCULATIONS
+            CalcWingWeight
+            
+            % STANDARD ATMOSPHERE CALCULATION
+            StandardAtmosphere
+            
+            % FLIGHT ENVELOPE 
+            CalcFlightEnvelope
+            
+            % GUST ENVELOPE
+            CalcGustEnvelope
+            
+            % FINAL ENVELOPE
+            CalcFinalEnvelope
+            
+            % BALANCING LOADS
+            CalcBalancingLoads
+            
+            % 3D INTERPOLATION OF DISTRIBUTIONS
+            CalcInterpDistrLiftDragPitchMom
+            
+            % SHEAR, BENDING, TORSION DISTRIBUTION 
+            CalcShearBendingTorsionDistr
+            
+            % UNSYMMETRICAL TORSION DUE TO AILERONS DEFLECTION
+            CalcUnsymmetricalLoads
+            
+            % HORIZONTAL TAIL CALCULATOR
+            CalcHorizontalTailLoads
+
+            % VERTICAL TAIL CALCULATOR 
+            CalcVerticalTailLoads
+            
+        case 'cs-23' 
+        case 'cs-25'
+    end
+end
---
+++ main_prog.m
@@ -0,0 +1,262 @@
+clear all; close all; clc;
+%% COMMENTO INIZIALE
+%  Prova del 18/07/2021 -- Implementazione della corretta variabile
+%  struttura 'Aircraft', come velivolo di prova è stato selezionato un
+%  sistema unmanned da certificare sotto la norma CS-VLA.
+
+% Log command window output. Avoid appending data to existing file.
+if exist('CMFlightLoads.txt','file')
+    diary OFF
+    delete CMFlightLoads.txt
+end
+diary CMFlightLoads.txt
+%% STORING THE WORKING DIRECTORY 
+dir = pwd;
+fprintf("--------------------------------------");
+fprintf('\n');
+fprintf("### Working directory ###"); 
+fprintf('\n');
+fprintf('%s\n', dir);
+fprintf("--------------------------------------");
+fprintf('\n\n');
+%% DEFINING A GLOBAL VARIABLE AIRCRAFT
+global Aircraft
+% Printing on screen the correct output
+fprintf("--------------------------------------");
+fprintf('\n');
+fprintf("--------------------------------------");
+fprintf('\n');
+fprintf("### Aircraft Flight Loads ###");
+fprintf('\n');
+fprintf("--------------------------------------");
+fprintf('\n');
+fprintf("--------------------------------------");
+fprintf('\n');
+fprintf('\n');
+%% Input specification - TLAR
+InputSource = "From File"; 
+% "From File"
+% "Custom"
+%do not edit
+% filename = 'Aircraft_test'; %do not edit
+% filename = 'drone_vla';     %do not edit
+filename = 'Tecnam_P92';  %do not edit
+
+%% INIZIALIZATION OF AircraftSTRUCT VARIABLE (Just the minimal values)
+cd initialization 
+% The 'dir' variable contains working directory path saved as a
+% char value
+dir = pwd;
+% Store working directory inside the log file
+fprintf('-----------------');
+fprintf('\n');
+fprintf('### Current directory ###');
+fprintf('\n');
+fprintf('%s\n', dir);
+% Aircraft = FlightLoadsInitialize();
+fprintf('\n');
+fprintf('Certification used from file.');
+% Aircraft = FromFileCertification_fun(Aircraft,filename);
+Aircraft = FromFileCertification_function(Aircraft,filename);
+fprintf('\n');
+pause(5/1000);
+
+switch(Aircraft.Certification.Attributes.airload_case.value) 
+    
+    case 'Schrenk'
+
+        %% CHORD CALCULATION 
+        cd ..
+        cd chord_calc 
+        % The 'dir' variable contains working directory path saved as a
+        % char value
+        dir = pwd;
+        % Store working directory inside the log file
+        fprintf('-----------------');
+        fprintf('\n');
+        fprintf('### Current directory ###');
+        fprintf('\n');
+        fprintf('%s\n', dir);
+        pause(5/1000);
+        % Chord calculation main program
+        test_calc_chord 
+
+        %% AERODYNAMIC MODEL
+        cd ..
+        cd calc_aero_model
+        % The 'dir' variable contains working directory path saved as a
+        % char value
+        dir = pwd;
+        % Store working directory inside the log file
+        fprintf('-----------------');
+        fprintf('\n');
+        fprintf('### Current directory ###');
+        fprintf('\n');
+        fprintf('%s\n', dir);
+
+        % SCRIPT TO EVALUATE AERODYNAMIC QUANTITIES 
+        CalcAeroModel
+        pause(5/1000);
+        
+        %% SCHRENK METHOD 
+        cd .. 
+        cd schrenk
+        % The 'dir' variable contains working directory path saved as a
+        % char value
+        dir = pwd;
+        % Store working directory inside the log file
+        fprintf('-----------------');
+        fprintf('\n');
+        fprintf('### Current directory ###');
+        fprintf('\n');
+        fprintf('%s\n', dir);
+        pause(5/1000);
+        % Schrenk method 
+        CalcSchrenkLoad
+
+        %% Pause
+        cd .. 
+        % The 'dir' variable contains working directory path saved as a
+        % char value
+        dir = pwd;
+        % Store working directory inside the log file
+        fprintf('-----------------');
+        fprintf('\n');
+        fprintf('### Current directory ###');
+        fprintf('\n');
+        fprintf('%s\n', dir);
+        pause(5/1000);    
+    
+    case 'OpenVSP'
+        %% Pause
+        cd .. 
+        % The 'dir' variable contains working directory path saved as a
+        % char value
+        dir = pwd;
+        % Store working directory inside the log file
+        fprintf('-----------------');
+        fprintf('\n');
+        fprintf('### Current directory ###');
+        fprintf('\n');
+        fprintf('%s\n', dir);
+        pause(5/1000);
+
+        %% STANDARD ATMOSPHERE 
+        cd csvla  
+        % The 'dir' variable contains working directory path saved as a
+        % char value
+        dir = pwd;
+        % Store working directory inside the log file
+        fprintf('-----------------');
+        fprintf('\n');
+        fprintf('### Current directory ###');
+        fprintf('\n');
+        fprintf('%s\n', dir);
+        % LAUNCH STANDARD ATMOSPHERE
+        StandardAtmosphere
+        pause(5/1000);
+        cd ..
+        % The 'dir' variable contains working directory path saved as a
+        % char value
+        dir = pwd;
+        % Store working directory inside the log file
+        fprintf('-----------------');
+        fprintf('\n');
+        fprintf('### Current directory ###');
+        fprintf('\n');
+        fprintf('%s\n', dir);
+
+        %% OPEN VSP CALCULATION
+        cd utilities
+        cd OpenVSP
+        % The 'dir' variable contains working directory path saved as a
+        % char value
+        dir = pwd;
+        % Store working directory inside the log file
+        fprintf('-----------------');
+        fprintf('\n');
+        fprintf('### Current directory ###');
+        fprintf('\n');
+        fprintf('%s\n', dir);
+        % Starting Open VSP calculation
+        Main_OPENVsp
+        % Storing results 
+        FromTableToStructAircraft
+        % Pause
+        pause(5/1000);
+
+        %% Pause
+        cd .. 
+        cd ..
+        % The 'dir' variable contains working directory path saved as a
+        % char value
+        dir = pwd;
+        % Store working directory inside the log file
+        fprintf('-----------------');
+        fprintf('\n');
+        fprintf('### Current directory ###');
+        fprintf('\n');
+        fprintf('%s\n', dir);
+        pause(5/1000);
+
+        %% CHORD CALCULATION 
+        cd chord_calc 
+        % The 'dir' variable contains working directory path saved as a
+        % char value
+        dir = pwd;
+        % Store working directory inside the log file
+        fprintf('-----------------');
+        fprintf('\n');
+        fprintf('### Current directory ###');
+        fprintf('\n');
+        fprintf('%s\n', dir);
+        pause(5/1000);
+        % Chord calculation main program
+        test_calc_chord
+
+        %% Pause
+        cd .. 
+        % The 'dir' variable contains working directory path saved as a
+        % char value
+        dir = pwd;
+        % Store working directory inside the log file
+        fprintf('-----------------');
+        fprintf('\n');
+        fprintf('### Current directory ###');
+        fprintf('\n');
+        fprintf('%s\n', dir);
+        pause(5/1000);
+
+        %% AERODYNAMIC MODEL
+        cd calc_aero_model 
+        % The 'dir' variable contains working directory path saved as a
+        % char value
+        dir = pwd;
+        % Store working directory inside the log file
+        fprintf('-----------------');
+        fprintf('\n');
+        fprintf('### Current directory ###');
+        fprintf('\n');
+        fprintf('%s\n', dir);
+
+        % SCRIPT TO EVALUATE AERODYNAMIC QUANTITIES 
+        CalcAeroModel
+        pause(5/1000);
+
+        %% Pause
+        cd .. 
+        % The 'dir' variable contains working directory path saved as a
+        % char value
+        dir = pwd;
+        % Store working directory inside the log file
+        fprintf('-----------------');
+        fprintf('\n');
+        fprintf('### Current directory ###');
+        fprintf('\n');
+        fprintf('%s\n', dir);
+        pause(5/1000);
+
+end
+
+%% APPLICATION OF AIRWORTHINESS REGULATIONS
+Aircraft = ApplyRegulation(Aircraft);
